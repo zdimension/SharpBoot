52619bdaa05d9a3e884d5f922602de96acb76853
zdimension <zippedfire@free.fr>
2015-11-25 17:20:11 +0100

    Forgot files

---
+++ SharpBoot/DropDownSelector.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SharpBoot
+{
+    partial class DropDownSelector
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.DisplayMember = "Disp";
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(0, 28);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(432, 23);
+            this.comboBox.TabIndex = 9;
+            this.comboBox.ValueMember = "Value";
+            // 
+            // DropDownSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox);
+            this.Name = "DropDownSelector";
+            this.Controls.SetChildIndex(this.comboBox, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
