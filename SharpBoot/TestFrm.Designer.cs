8f9b65150c1d8d3d65d8b2d08bc5859247ef17c8
zdimension <zippedfire@free.fr>
2015-12-15 19:37:20 +0100

    test work

--- SharpBoot/TestFrm.Designer.cs
+++ SharpBoot/TestFrm.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.customTextBox1 = new SharpBoot.CustomTextBox();
+            this.SuspendLayout();
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.customTextBox1.Location = new System.Drawing.Point(53, 104);
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Size = new System.Drawing.Size(180, 23);
+            this.customTextBox1.TabIndex = 0;
+            // 
+            // TestFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.customTextBox1);
+            this.Name = "TestFrm";
             this.Text = "TestFrm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private CustomTextBox customTextBox1;
     }
 }
